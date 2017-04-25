52816841502727f59ff3a5331deea39d32bc28d9
Stephen Weber <stephen.weber3@gmail.com>
2017-04-25 19:31:12 -0400

    Update web.xml and DBUtil

--- src/java/DB/DBUtil.java
+++ src/java/DB/DBUtil.java
@@ -5,10 +5,14 @@
  */
 package DB;
 
-/**
- *
- * @author Stephen
- */
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class DBUtil {
+    private static final EntityManagerFactory emf =
+            Persistence.createEntityManagerFactory("WxForecasterPU");
     
+    public static EntityManagerFactory getEmFactory() {
+        return emf;
+    }
 }
