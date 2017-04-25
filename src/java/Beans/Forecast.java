e14fd9246dc127fbf0b0f862010e8cc74bc37168
Stephen Weber <stephen.weber3@gmail.com>
2017-04-25 19:26:21 -0400

    Add User and Forecast beans

--- src/java/Beans/Forecast.java
+++ src/java/Beans/Forecast.java
@@ -5,10 +5,62 @@
  */
 package Beans;
 
+import java.io.Serializable;
+
 /**
  *
  * @author Stephen
  */
-public class Forecast {
+public class Forecast implements Serializable{
+    
+    private int highTemp;
+    private int lowTemp;
+    private int windSpeed;
+    private String skyConditions;
+    private double precip;
+    
+    public Forecast(){
+        
+    }
+    
+    public void setHighTemp(int highTemp){
+        this.highTemp = highTemp;
+    }
+    
+    public int getHighTemp(){
+        return highTemp;
+    }
+    
+    public void setLowTemp(int lowTemp){
+        this.lowTemp = lowTemp;
+    }
+    
+    public int getLowTemp(){
+        return lowTemp;
+    }
+    
+    public void setWindSpeed(int windSpeed){
+        this.windSpeed = windSpeed;
+    }
+    
+    public int getWindSpeed(){
+        return windSpeed;
+    }
+    
+    public void setSkyConditions(String skyConditions){
+        this.skyConditions = skyConditions;
+    }
+    
+    public String getSkyConditions(){
+        return skyConditions;
+    }
+    
+    public void setPrecip(double precip){
+        this.precip = precip;
+    }
+    
+    public double getPrecip(){
+        return precip;
+    }
     
 }
